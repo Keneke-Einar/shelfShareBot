abdd873db95ff100499f25d54cafca1324f8b3ab
Keneke-Einar <97710288+Keneke-Einar@users.noreply.github.com>
2022-10-26 12:31:20 +0600

    message with btns bug fix

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 from tokenize import Token
 
+ADMIN = []
 TOKEN = '5767406744:AAGNAA2Nou3SuGEegsLA9n4_0lXr8v1sgc8'
--- main.py
+++ main.py
@@ -1,27 +1,39 @@
+from datetime import time
+from telebot import apihelper, types
 import telebot
 import config
-import adminPanel
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=['find'])
-def start(message):
-    bot.send_message(message.chat.id, "This is find menu")
 
-@bot.message_handler(commands=['rents'])
-def start(message):
-    bot.send_message(message.chat.id, "This is menu of a book that u are renting now")
+# handlers
 
-@bot.message_handler(commands=['search'])
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "There will be list of all available books")
+    markup = types.InlineKeyboardMarkup(row_width=2)
 
-@bot.message_handler(commands=['adminCheck'])
-def start(message):
-    bot.send_message(message.chat.id, "Keneke, write ur password")
-    adminPanel()
+    btn1 = types.InlineKeyboardButton("User", callback_data='user')
+    btn2 = types.InlineKeyboardButton("Admin", callback_data='user')
 
+    markup.add(btn1, btn2)
+
+    mess = f"<b>Enter as</b>:"
+    bot.send_message(message.chat.id, mess, reply_markup=markup)
+
+
+@bot.message_handler(commands=['idCheck'])
+def idCheck(message):
+    bot.send_message(message.chat.id, f"ID is - {message.from_user.id}")
 
 # run()
 
-bot.polling(none_stop=True)
+while True:
+    try:
+        bot.polling(none_stop=True)
+        apihelper.proxy = {
+            'https': 'socks5h://165.227.79.99:7497'
+        }
+
+    except Exception as e:
+        print(e)
+        time.sleep(15)
